3237dad2cc2ce6811571a51757a24e320d076a33
Shijal Mishra <txb16077@gmail.com>
2025-12-05 09:22:15 +0530

    learned set interval

---
+++ templates/MINE/DAY5-CD/HW/second.js
@@ -0,0 +1,18 @@
+let arr1=[11,2,23];
+let [e,f,g]=arr1;
+console.log(a);
+console.log(b); 
+console.log(c);
+
+
+let arr=[11,2,23];
+parseInt(arr.sort());
+console.log(arr); 
+
+// we shouldnt give different name instead of na in the printing statement.
+let stu={na,name:"shijal",city:"ktm"};
+let{na:a,city:b}=stu;
+console.log(a);
+console.log(b);
+
+
---
+++ templates/MINE/DAY5-CD/HW/setinterval.js
@@ -0,0 +1,7 @@
+console.log("one");
+setInterval(() => {
+    console.log("Two");
+},2000);
+console.log("three");
+
+
