5bb2f53976de4fa145905470b5982bfbf6e401b0
Shijal Mishra <txb16077@gmail.com>
2025-12-08 10:29:12 +0530

    learned the basic syntax

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -4,11 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+let city="kovai";
+let nums=[11,22,33];
   return (
     <>
    <h1>React is Not a framework, it is a library framework</h1>
+   <p>I am from {city}</p>
+   <p>Lucky numbers {nums}</p>
+   <p>
+  {1==2}
+   </p>
+   {/* cant print directly like object like <p>{name:"Rohit",age:40}</p> */}
+   <p>{JSON.stringify({name:"Rohit",age:40})}</p>
     </>
   )
 }
