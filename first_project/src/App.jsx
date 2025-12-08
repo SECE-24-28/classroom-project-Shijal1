cadba657169647107fc9b0b0d6af7ef20b76f222
Shijal Mishra <txb16077@gmail.com>
2025-12-08 12:01:12 +0530

    learned the basic

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -2,22 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import Content from'./Content'
+
 
 function App() {
-let city="kovai";
-let nums=[11,22,33];
   return (
     <>
-   <h1>React is Not a framework, it is a library framework</h1>
-   <p>I am from {city}</p>
-   <p>Lucky numbers {nums}</p>
-   <p>
-  {1==2}
-   </p>
-   {/* cant print directly like object like <p>{name:"Rohit",age:40}</p> */}
-   <p>{JSON.stringify({name:"Rohit",age:40})}</p>
-    </>
-  )
+<Header />
+<Content />
+</>
+)
 }
 
 export default App
---
+++ first_project/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+        let des={background:"black",color:"white",padding:"10px"}
+    return (
+        <div>
+            <h1 style={des}>Header Component</h1>
+            <p>Welcome to the React Event Handling Example</p>
+
+        </div>
+    )
+}
+
+export default Header
