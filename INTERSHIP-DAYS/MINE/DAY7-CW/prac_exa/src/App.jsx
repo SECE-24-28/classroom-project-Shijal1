306cc374aa439e7c85d14772586f4fefd4afdc9d
Shijal Mishra <txb16077@gmail.com>
2025-12-09 14:50:59 +0530

    added all the day7 work

--- INTERSHIP-DAYS/MINE/DAY7-CW/prac_exa/src/App.jsx
+++ INTERSHIP-DAYS/MINE/DAY7-CW/prac_exa/src/App.jsx
@@ -2,34 +2,63 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddStudent from './AddStudent'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() 
+{
+    const [stuList,setStuList]=useState([
+      {id:1,sname:"Jack",fee:true},
+      {id:2,sname:"Smith",fee:false},
+      {id:3,sname:"Victor",fee:true}
+    ])
 
+    const handleDelete=(id)=>
+    {
+      const newList=stuList.filter((stu)=>stu.id!==id)
+      setStuList(newList)
+    }
+    const handleChange=(id)=>
+    {
+      const newList=stuList.map((stu)=>(stu.id===id)?({...stu,fee:!stu.fee}):(stu))
+      setStuList(newList)
+    }
+   const addItem=(item)=>
+   {
+    const sz=stuList.length-1;
+     const nid=(stuList.length)?(stuList[sz].id+1):(1)
+     const newObj={id:nid,sname:item,fee:false}
+     const newList=[...stuList,newObj]
+     setStuList(newList)
+   }
+   const handleSubmit=(e)=>
+   {
+    e.preventDefault()
+    addItem(newstu)
+    setNewStu('')
+   }
+    const [newstu,setNewStu]=useState('')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <h1>Students List</h1>
+       <AddStudent  newstu={newstu}
+                    setNewStu={setNewStu}
+                    handleSubmit={handleSubmit}
+       />
+       <ul>
+       {
+        stuList.map((stu)=>
+          <li key={stu.id}>
+            <input type="checkbox" checked={stu.fee}
+                                    onChange={()=>handleChange(stu.id)}
+            />
+            <label>{stu.sname}</label>
+            <button onClick={()=>handleDelete(stu.id)}>Delete</button>
+          </li>
+        )
+       }
+       </ul>
     </>
   )
 }
 
-export default App
+export default App
