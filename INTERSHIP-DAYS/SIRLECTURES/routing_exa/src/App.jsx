a9cb41cb627917e70fa5fe28f99b788edc29f308
Shijal Mishra <txb16077@gmail.com>
2025-12-10 14:09:58 +0530

    added

--- INTERSHIP-DAYS/SIRLECTURES/routing_exa/src/App.jsx
+++ INTERSHIP-DAYS/SIRLECTURES/routing_exa/src/App.jsx
@@ -2,34 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Home'
+import About from './About'
+import Gallary from './Gallary'
+import PostPage from './PostPage'
+import { Link, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        <li> <link to="/">Home</link></li>
+        <li> <link to="/about">About</link></li>
+        <li> <link to="/gallary">Gallary</link></li>
+        <li> <link to="/postpage">PostPage</link></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallary" element={<Gallary />} />
+        <Route path="/postpage" element={<PostPage />}>
+          <Route path=":id" element={<Post />} />
+          <Route path="newpost" element={<NewPost />} />
+        </Route>
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App
---
+++ INTERSHIP-DAYS/SIRLECTURES/routing_exa/src/PostPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <div>
+        <ol>
+            <li> <Link> Post 1 </Link></li>
+            <li></li>
+        </ol>
+    </div>
+  )
+}
+
+export default PostPage
