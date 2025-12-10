3865afd3e8cb37b6f1fda50291db3b2eb2c39b1f
Shijal Mishra <txb16077@gmail.com>
2025-12-10 15:03:04 +0530

    Added the all

--- INTERSHIP-DAYS/SIRLECTURES/counterapp/src/App.jsx
+++ INTERSHIP-DAYS/SIRLECTURES/counterapp/src/App.jsx
@@ -8,9 +8,18 @@ function App() {
       <h2>Counter App</h2>
       <h1>{count}</h1>
 
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() =>
+         setCount(count + 1)
+         }>Increase
+          </button>
+      <button onClick={() =>
+         setCount(count - 1)
+         }>Decrease
+         </button>
+      <button onClick={() => 
+        setCount(0)
+        }>Reset
+        </button>
     </div>
   )
 }
--- INTERSHIP-DAYS/SIRLECTURES/eventhandling/src/App.jsx
+++ INTERSHIP-DAYS/SIRLECTURES/eventhandling/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let city = "Hetauda";
+  let bums = [11, 22, 33];
+  let age = 19;
+
+  const [num, setNum] = useState(50);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>I am from {city}</h1>
+      <p>Lucky numbers: {bums.join(", ")}</p>
+      <p>{true ? "TRUE" : "FALSE"}</p>
+      <p>{JSON.stringify({ name: "Shijal", age: 19 })}</p>
+      <p>{age >= 18 ? "ADULT" : "MINOR"}</p>
+
+      <h2>State value: {num}</h2>
+
+      {/* Increase Button */}
+      <button onClick={() => setNum(num + 1)}>
+        Increase
+      </button>
+
+      {/* Decrease Button */}
+      <button onClick={() => setNum(num - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
